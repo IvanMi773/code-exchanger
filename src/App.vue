a39ab97bae0acef23db8e758682c4d55e5258911
IvanMi773 <imihajlovskij23@gmail.com>
2020-05-02 18:22:34 +0300

    create footer

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,21 @@
 		<div id="nav">
 			<Navbar />
 		</div>
+
 		<router-view />
+
+		<Footer />
 	</div>
 </template>
 
 <script>
 	import Navbar from '@/components/App/Navbar';
+	import Footer from '@/components/App/Footer';
 
 	export default {
 		components: {
 			Navbar,
+			Footer,
 		},
 	};
 </script>
---
+++ src/components/App/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer class="page-footer">
+		<div class="footer-copyright">
+			<div class="container">
+				© 2020 Всі права захищені
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+	export default {
+		name: 'Footer',
+	};
+</script>
+
+<style lang="scss" scoped>
+	@import '@/assets/scss/index';
+
+	.page-footer {
+		background-color: $black;
+		position: fixed;
+		left: 0px;
+		bottom: 0px;
+		height: 74px;
+		width: 100%;
+	}
+</style>
