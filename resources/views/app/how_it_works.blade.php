07246693d2d334fdd3270cb4b30465b374848cb3
IvanMi773 <imihajlovskij23@gmail.com>
2020-06-22 17:23:26 +0300

    how it works page design

--- resources/views/app/how_it_works.blade.php
+++ resources/views/app/how_it_works.blade.php
@@ -1,7 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        
+    <div class="bg p-5">
+        <div class="text-center">
+            <h1 class="font-weight-bold">
+                Як почати працювати з Code Exchanger?
+            </h1>
+
+            <h5 class="font-weight-light text-muted mt-4">
+                Code Exchanger потрібен для того щоб створювати невеликі проекти у невеликих командах
+            </h5>
+        </div>
+    </div>
+
+    <div class="text-center mt-4">
+        <h2 class="font-weight-bold">
+            Як це працює?
+        </h2>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-6">
+            <h5 class="text-center">
+                1. Ви в команді створюєте завдання і виконуєте їх
+            </h5>
+        </div>
+
+        <div class="col-6">
+            <h5 class="text-center">
+                2. Ваш тімлід збере ці завдання в один проект 
+            </h5>
+        </div>
+    </div>
+
+    <div class="text-center mt-4">
+        <h2 class="font-weight-bold">
+            Що потрібно щоб почати роботу з Code Exchanger?
+        </h2>
+
+        <h5>
+            1. Зарєеструйтесь або увійдіть
+        </h5>
+
+        <h5>
+            2. Виберіть групу або створіть свою
+        </h5>
+
+        <h5>
+            3. Створюйте завдання та додавайте свій код щоб інші учасники групи змогли його побачити
+        </h5>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Other routes
 Route::auth();
 
 Route::get('/', 'App\AppController@index')->name('welcome');
 Route::get('/how-it-works', 'App\AppController@howItWorks')->name('howItWorks');
 
+// Group routes
 Route::get('/group/choose', 'App\GroupController@choose')->name('group.choose');
 Route::get('/group/create', 'App\GroupController@create')->name('group.create');
 Route::get('/group/enter', 'App\GroupController@enter')->name('group.enter');
 Route::post('/group/store', 'App\GroupController@store')->name('group.store');
-Route::get('/group/home/{group}', 'App\GroupController@show')
-    ->name('group.show')
-    ->middleware('canSeeGroupPage');
-Route::get('/group/edit/{group}', 'App\GroupController@edit')->name('group.edit');
-Route::post('/group/update/{group}', 'App\GroupController@update')->name('group.update');
-Route::delete('/group/delete/{group}', 'App\GroupController@destroy')->name('group.delete');
-Route::delete('/group/user/delete/{group}', 'App\GroupController@groupUserDelete')->name('group.user.delete');
-Route::post('/group/user/add/{group}', 'App\GroupController@addUser');
 
+Route::middleware('canSeeGroupPage')->group(function () {
+    Route::get('/group/home/{group}', 'App\GroupController@show')->name('group.show');
+    Route::get('/group/edit/{group}', 'App\GroupController@edit')->name('group.edit');
+    Route::post('/group/update/{group}', 'App\GroupController@update')->name('group.update');
+    Route::delete('/group/delete/{group}', 'App\GroupController@destroy')->name('group.delete');
+    Route::delete('/group/user/delete/{group}', 'App\GroupController@groupUserDelete')->name('group.user.delete');
+    Route::post('/group/user/add/{group}', 'App\GroupController@addUser');
+});
+
+// Task routes
 Route::get('/task/create', 'App\TaskController@create');
 Route::post('/task/store', 'App\TaskController@store')->name('task.store');
 Route::get('/task/{task}', 'App\TaskController@show');
